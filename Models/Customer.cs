548a8defcffe06dfbb27aba07ae7c14ec5625969
msejuro312 <msejuro312@protonmail.com>
2026-07-25 12:51:27 -0500

    clases completas

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,6 +1,18 @@
-﻿namespace WebApplication1.Models
+﻿using System.Diagnostics.Metrics;
+using System.Net;
+
+namespace WebApplication1.Models
 {
     public class Customer
     {
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }        
+        public string Country { get; set; }
+        public string Phone { get; set; } 
+        
     }
 }
